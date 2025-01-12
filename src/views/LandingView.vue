51e3b2a1fb56324807f514bf109ac07b30c1af7f
Ferlow <hello.flobauch@gmail.com>
2025-01-12 13:19:21 +0000

    minor changes on landing

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -13,21 +13,22 @@
         </p>
         <p class="instructions">
           For usage instructions and tutorial videos, head to: <a href="https://github.com/Flokedex/Wiki-GHpages" target="_blank" rel="noopener noreferrer">
-          Flokédex Wiki
-          </a>
+          Flokédex Wiki</a>.
         </p>
         <p class="contribute">
-          This project is a complex open source app available at
-          <a href="https://github.com/Flokedex" target="_blank" rel="noopener noreferrer">
-          Flokédex GitHub Repository
-          </a>, where you can contribute to make this better.
+          This project is open source and available at
+          <a href="https://github.com/Flokedex" target="_blank" rel="noopener noreferrer">Flokédex GitHub Repository</a>, where you can contribute to make this better.
         </p>
         <p class="support">
-          If you like my app, you can show your appreciation by buying me a coffee:
-          <a href="https://buymeacoffee.com/ferlow" target="_blank" rel="noopener noreferrer">
-            Buy Me a Coffee
-          </a>
+          If you like my app, you can show your appreciation
+          <a href="https://buymeacoffee.com/ferlow" target="_blank" rel="noopener noreferrer">by buying me a coffee</a>.
         </p>
+        <p class="disclaimer">
+          This app is in its early stages and bugs are guarenteed. Head to the repo to report them.
+        </p>
+          <p>
+            It is also not affiliated with Nintendo, Game Freak, or The Pokémon Company.
+          </p>
 
         <button class="discord-login-button" @click="$loginWithDiscord">
   <img src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/636e0a6ca814282eca7172c6_icon_clyde_white_RGB.svg" alt="Discord Icon" style="width: 20px; height: 20px; margin-right: 8px;">
